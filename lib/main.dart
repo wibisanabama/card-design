eeb545c246933e663af769574f0ddd0ed31a78e4
wibisanabama <wibisanabama@gmail.com>
2026-04-26 06:53:39 +0700

    Refactor MainApp to include multiple card widgets and enhance layout structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:card_design/awan_card.dart';
+import 'package:card_design/food_card.dart';
+import 'package:card_design/lorem_card.dart';
+import 'package:card_design/menu_card.dart';
+import 'package:card_design/title_card.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +15,29 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CardDesign(),
+                  SizedBox(width: 5),
+                  FoodCard(),
+                  SizedBox(width: 5),
+                  LoremCard(),
+                  SizedBox(width: 5),
+                  AwanCard(),
+                  SizedBox(width: 5),
+                ],
+              ),
+              TitleCard(),
+            ],
+          ),
         ),
       ),
     );
